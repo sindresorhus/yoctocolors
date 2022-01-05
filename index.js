f86e09910ce0af7dea4d3cb89c159a316ff00579
fisker Cheung <lionkay@gmail.com>
2022-01-05 17:23:35 +0800

    Reduce size

--- index.js
+++ index.js
@@ -6,32 +6,35 @@ const hasColors = tty.WriteStream.prototype.hasColors();
 // Intentionally not using template literal for performance.
 const format = (startCode, endCode) => hasColors ? string => '\u001B[' + startCode + 'm' + string + '\u001B[' + endCode + 'm' : string => string;
 
-export const reset = format(0, 0);
-export const bold = format(1, 22);
-export const dim = format(2, 22);
-export const italic = format(3, 23);
-export const underline = format(4, 24);
-export const overline = format(53, 55);
-export const inverse = format(7, 27);
-export const hidden = format(8, 28);
-export const strikethrough = format(9, 29);
+// eslint-disable-next-line one-var
+export const
+	reset = format(0, 0),
+	bold = format(1, 22),
+	dim = format(2, 22),
+	italic = format(3, 23),
+	underline = format(4, 24),
+	overline = format(53, 55),
+	inverse = format(7, 27),
+	hidden = format(8, 28),
+	strikethrough = format(9, 29),
 
-export const black = format(30, 39);
-export const red = format(31, 39);
-export const green = format(32, 39);
-export const yellow = format(33, 39);
-export const blue = format(34, 39);
-export const magenta = format(35, 39);
-export const cyan = format(36, 39);
-export const white = format(37, 39);
-export const gray = format(90, 39);
+	black = format(30, 39),
+	red = format(31, 39),
+	green = format(32, 39),
+	yellow = format(33, 39),
+	blue = format(34, 39),
+	magenta = format(35, 39),
+	cyan = format(36, 39),
+	white = format(37, 39),
+	gray = format(90, 39),
 
-export const bgBlack = format(40, 49);
-export const bgRed = format(41, 49);
-export const bgGreen = format(42, 49);
-export const bgYellow = format(43, 49);
-export const bgBlue = format(44, 49);
-export const bgMagenta = format(45, 49);
-export const bgCyan = format(46, 49);
-export const bgWhite = format(47, 49);
-export const bgGray = format(100, 49);
+	bgBlack = format(40, 49),
+	bgRed = format(41, 49),
+	bgGreen = format(42, 49),
+	bgYellow = format(43, 49),
+	bgBlue = format(44, 49),
+	bgMagenta = format(45, 49),
+	bgCyan = format(46, 49),
+	bgWhite = format(47, 49),
+	bgGray = format(100, 49);
+
